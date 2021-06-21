fcaef04bccc17d436dc868e66453bc6fc3efc574
Sydney Gibbon <sydneymackgibbon@gmail.com>
2021-06-21 12:25:21 -0300

    Added tarifas navar etc

---
+++ registrarventadeentrada/createTarifas.py
@@ -0,0 +1,25 @@
+from datetime import date
+
+tarifas = [
+    {
+        'fechaFinVigencia' : date(2023, 7, 10),
+        'monto' : 3000,
+        'montoAdicionalGuia': 1000
+    },
+    {
+        'fechaFinVigencia' : date(2023, 7, 11),
+        'monto' : 4000,
+        'montoAdicionalGuia': 1000
+    },
+    {
+        'fechaFinVigencia' : date(2023, 7, 12),
+        'monto' : 4500,
+        'montoAdicionalGuia': 1000
+    },
+    ] 
+    
+    from myApp.models import Tarifa
+
+    for tarifa in tarifas:
+        obj = Tarifa.objects.create(**tarifa)
+        print(obj.pk)
